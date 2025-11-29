a97d0883bfa990dfc086ca3ffa53a5816f8c3cdb
bhavaninagasankar <bhavaninagasankar@gmail.com>
2025-11-29 19:40:24 +0530

    Setup for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/hackathon/',
+  base: '/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
